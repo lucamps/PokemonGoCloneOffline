028682d192afaa792886f162bf4d559c3860db85
Rodrigo Chichorro <reobcnegf@gmail.com>
2020-11-01 20:23:41 -0300

    Apertar "Aceitar" desasbilita "Alterar" e "Rejeitar" reabilita

--- app/src/main/java/teste/lucasvegi/pokemongooffline/Controller/TrocaPropostaActivity.java
+++ app/src/main/java/teste/lucasvegi/pokemongooffline/Controller/TrocaPropostaActivity.java
@@ -4,23 +4,37 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 
 import teste.lucasvegi.pokemongooffline.R;
 
 public class TrocaPropostaActivity extends Activity{
+
+    Button alterar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_troca_proposta);
 
-        //obtem referências das views
-
         try {
-
+            alterar = (Button) findViewById(R.id.botaoAlterar);
 
         }catch (Exception e){
             Log.e("TROCA", "ERRO: " + e.getMessage());
         }
+    }
+
+    public void alterarOfertaTroca(View v){
+
+    }
+
+    public void aceitarTroca(View v){
+        alterar.setEnabled(false);
+    }
+
+    public void rejeitarTroca(View v){
+        alterar.setEnabled(true);
     }
 
     public void clickVoltar(View v){
