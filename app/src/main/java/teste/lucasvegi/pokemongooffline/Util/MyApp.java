baf9387c4fc648afbac0566f6d5ca0fcb59f651a
Lucas <lucas.tx.reis@gmail.com>
2020-11-22 14:30:39 -0300

    Socket send,get pokemon methods

--- app/src/main/java/teste/lucasvegi/pokemongooffline/Util/MyApp.java
+++ app/src/main/java/teste/lucasvegi/pokemongooffline/Util/MyApp.java
@@ -3,6 +3,10 @@ package teste.lucasvegi.pokemongooffline.Util;
 import android.app.Application;
 import android.bluetooth.BluetoothSocket;
 import android.content.Context;
+import android.util.Log;
+
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 
 /**
  * Created by Lucas on 12/12/2016.
@@ -30,4 +34,30 @@ public class MyApp extends Application {
     public static void setBluetoothSocket(BluetoothSocket socket){
         MyApp.bluetoothSocket = socket;
     }
+
+    public static void setPokemon(int id) {
+
+        // Criando stream de dados para o socket(host, port) em @param bluetoothSocket
+        try {
+            ObjectOutputStream out = new ObjectOutputStream(bluetoothSocket.getOutputStream());
+            out.writeInt(id);
+        } catch (Exception e) {
+            Log.e("SOCKET", "Falha ao enviar o pokémon");
+        }
+
+    }
+    public static int getPokemon() {
+
+        // Recebendo o stream de dados
+        try {
+            ObjectInputStream out = new ObjectInputStream(bluetoothSocket.getInputStream());
+            int id = out.readInt();
+            return id;
+        } catch (Exception e) {
+            Log.e("SOCKET", "Falha ao receber o pokémon");
+        }
+
+        return -1;
+    }
+
 }
