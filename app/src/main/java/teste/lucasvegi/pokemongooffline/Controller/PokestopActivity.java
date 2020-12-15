8f6aa6b7d5ac7fe193598b86f1f825e760f5f20c
wallace-f-rosa <wferancini@gmail.com>
2020-12-15 15:31:26 -0300

    imprimindo toasts de xp e ovo sem sobreposição
    adição de toast para indicar limite de ovos

--- app/src/main/java/teste/lucasvegi/pokemongooffline/Controller/PokestopActivity.java
+++ app/src/main/java/teste/lucasvegi/pokemongooffline/Controller/PokestopActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.database.Cursor;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -18,6 +19,7 @@ import teste.lucasvegi.pokemongooffline.Model.InteracaoPokestop;
 import teste.lucasvegi.pokemongooffline.Model.Pokestop;
 import teste.lucasvegi.pokemongooffline.R;
 import teste.lucasvegi.pokemongooffline.Util.BancoDadosSingleton;
+import teste.lucasvegi.pokemongooffline.Util.MyApp;
 
 public class PokestopActivity extends Activity {
     private TextView placeName;
@@ -82,6 +84,7 @@ public class PokestopActivity extends Activity {
     }
 
     public void PegaOvo(View view) {
+        Pegou = false;
         Date TempoAtual = Calendar.getInstance().getTime();
 
         InteracaoPokestop inter = ControladoraFachadaSingleton.getInstance().getUltimaInteracao(Pkstp);
@@ -96,18 +99,33 @@ public class PokestopActivity extends Activity {
             if(diffSec > 300){
 
                 ControladoraFachadaSingleton.getInstance().interagePokestop(Pkstp, TempoAtual);
-                Toast.makeText(this,"Pegou Ovo ", Toast.LENGTH_LONG).show();
 
                 //Pega o ovo
                 Pegou = true;
             }
-            else
-                Toast.makeText(this,"Espere mais "+ String.valueOf(300-diffSec) +" segundos",Toast.LENGTH_SHORT).show();
+            else{
+                Toast toastEspere = Toast.makeText(MyApp.getAppContext(),"Espere mais "+ String.valueOf(300-diffSec) +" segundos",Toast.LENGTH_SHORT);
+                toastEspere.setGravity(Gravity.TOP | Gravity.CENTER_HORIZONTAL,0,0);
+                toastEspere.show();
+            }
+
         }
 
-        if(Pegou)
-            Toast.makeText(this,"Pegou Ovo ",Toast.LENGTH_LONG).show();
+        if(Pegou) {
+            Integer xp = ControladoraFachadaSingleton.getInstance().getXpEvento("pokestop");
+            Integer ovos = ControladoraFachadaSingleton.getInstance().getOvos().size();
+            if(ovos > 8){
+                Toast toastInventario = Toast.makeText(MyApp.getAppContext(), "Inventário de ovos está cheio!", Toast.LENGTH_LONG);
+                toastInventario.setGravity(Gravity.TOP | Gravity.CENTER_HORIZONTAL,0,0);
+                toastInventario.show();
+            }
+            else{
+                Toast toastOvo = Toast.makeText(MyApp.getAppContext(), "Pegou Ovo ", Toast.LENGTH_LONG);
+                toastOvo.setGravity(Gravity.TOP | Gravity.CENTER_HORIZONTAL,0,0);
+                toastOvo.show();
+            }
 
+        }
     }
 
 }
